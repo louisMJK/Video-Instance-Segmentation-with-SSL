53ac2d79a9aa12038be2eed95a9f920ff6d9223a
Yunlei (Louis) Lu <luyunlei99@gmail.com>
2023-04-26 12:29:13 -0400

    add BYOL load weights from trained model

--- ssl/code/scripts/distributed-train.sh
+++ ssl/code/scripts/distributed-train.sh
@@ -1,7 +1,8 @@
 torchrun --nproc_per_node=4 ../train.py \
+    --use-trained \
     --backbone resnet50 \
     --optim lars \
     --sched cosine \
-    --lr-base 1e-2 \
+    --lr-base 1e-3 \
     -b 64 --workers 16 \
-    --epochs 100 \
+    --epochs 30 \
