6f642d3c7b08f821d92539c350649a2a8eab80da
Yunlei (Louis) Lu <luyunlei99@gmail.com>
2023-04-29 14:26:08 -0400

    inference model

--- inference/models/large_model.py
+++ inference/models/large_model.py
@@ -1,3 +1,5 @@
+from torch import nn
+from torchvision import models
 from simvp_model import SimVP_Model
 
 
@@ -7,11 +9,16 @@ class LargeModel(nn.Module):
     def __init__(self):
         super(LargeModel, self).__init__()
         self.predictor = SimVP_Model()
-
+        self.fcn_resnet = models.segmentation.fcn_resnet50(
+            weights=None,
+            weights_backbone=None,
+            num_classes=49,
+            aux_loss=True,
+        )
 
 
     def forward(self, x, **kwargs):
         x = self.predictor(x)
-        #TODO: add the rest of the model
-
+        x = self.fcn_resnet(x)
         return x
+
