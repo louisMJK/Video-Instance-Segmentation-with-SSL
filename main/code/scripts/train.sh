e1591d2e3916004bd8d4ff917da8fb00ae5b425f
Yunlei (Louis) Lu <luyunlei99@gmail.com>
2023-04-29 02:32:33 -0400

    shit

---
+++ main/code/scripts/train.sh
@@ -0,0 +1,5 @@
+python3 ../train.py \
+    --use-fcn-head \
+    --freeze-backbone --freeze-fcn \
+    --lr-base 1e-2 \
+    --epochs 10 \
--- ssl/code/scripts/distributed-train.sh
+++ ssl/code/scripts/distributed-train.sh
@@ -1,7 +1,8 @@
 torchrun --nproc_per_node=4 ../train.py \
     --backbone resnet50 \
-    --optim lars \
-    --sched cosine \
-    --lr-base 1e-2 \
+    --use-trained --model-dir '../../output/backbone-resnet50-0.9167/byol_best.pth' \
+    --optim sgd \
+    --sched exp --lr-decay 0.9 \
+    --lr-base 1e-3 \
     -b 64 --workers 16 \
-    --epochs 40 \
+    --epochs 20 \
