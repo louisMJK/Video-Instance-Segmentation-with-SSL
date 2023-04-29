b99d8f5c45526c7171487f5f65bc3380f06c961f
Yirong Bian <brettb@b-10-27-89-8.dynapool.vpn.nyu.edu>
2023-04-29 13:26:37 -0400

    add inference

--- inference/large_model.py
+++ inference/large_model.py
@@ -1,1 +1,17 @@
-from simvp_model import SimVP
+from simvp_model import SimVP_Model
+
+
+class LargeModel(nn.Module):
+    """Large Model for the End to End Task"""
+
+    def __init__(self):
+        super(LargeModel, self).__init__()
+        self.predictor = SimVP_Model()
+
+
+
+    def forward(self, x, **kwargs):
+        x = self.predictor(x)
+        #TODO: add the rest of the model
+
+        return x
