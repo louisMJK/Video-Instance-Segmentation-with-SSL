7442bdef9ef7711539e9392b6460e7110523e5ab
Yirong Bian <brettb@b-10-27-89-8.dynapool.vpn.nyu.edu>
2023-04-29 15:28:37 -0400

    edit gsta script

--- gsta/scripts/distributed-train.sh
+++ gsta/scripts/distributed-train.sh
@@ -1,9 +1,12 @@
-torchrun --nproc_per_node=4 ../train.py \
+torchrun --nproc_per_node=4 ../train2.py \
     --lr-base 1e-4 \
     --optim adam \
     -b 4 --workers 16 \
     --epochs 40 \
     --hid-S 64\
     --hid-T 512\
-    --data-dir '../../dl/dataset/'\
-    --amp
+    --data-dir '../../Dataset_Student/'\
+    --best-model '../model_best.pth'\
+    --drop-path 0.05 \
+    --amp \
+    --next-n-frame 6\
