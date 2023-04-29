0a4ffdd24a79690d39d6100a951dc8722b0538f9
Letao Chen <lc5187@log-3.hpc.nyu.edu>
2023-04-29 01:44:08 -0400

    change of amp and batch

--- gsta/scripts/distributed-train.sh
+++ gsta/scripts/distributed-train.sh
@@ -1,9 +1,10 @@
 torchrun --nproc_per_node=4 ../train.py \
-    --lr-base 1e-3 \
+    --lr-base 1e-5 \
     --optim adam \
-    -b 4 --workers 16 \
-    --epochs 10 \
+    -b 16 --workers 16 \
+    --epochs 3 \
     --hid-S 64\
     --hid-T 512\
     --data-dir '../../Dataset_Student/'\
-    --best-model 'best_model.pth'
+    --best-model 'best_model.pth'\
+    --amp
