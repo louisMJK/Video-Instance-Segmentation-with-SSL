1a98bad0e9f5518f6a88d1f0517aa1c9ac95ebb9
Letao Chen <lc5187@log-3.hpc.nyu.edu>
2023-04-29 16:06:35 -0400

    update on sh

--- gsta/scripts/distributed-train.sh
+++ gsta/scripts/distributed-train.sh
@@ -1,12 +1,12 @@
 torchrun --nproc_per_node=4 ../train2.py \
-    --lr-base 1e-4 \
+    --lr-base 1e-5 \
     --optim adam \
     -b 4 --workers 16 \
-    --epochs 40 \
+    --epochs 30 \
     --hid-S 64\
     --hid-T 512\
     --data-dir '../../Dataset_Student/'\
     --best-model '../model_best.pth'\
-    --drop-path 0.05 \
+    --drop-path 0.1 \
     --amp \
-    --next-n-frame 6\
+    --next-n-frame 8\
